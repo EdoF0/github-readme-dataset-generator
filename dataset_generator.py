811b6a99d4f9e2a136737fa218b99664aab55853
Edoardo Fava <f.edoardo.0@gmail.com>
2023-12-11 16:11:53 +0100

    dataset generator settings print and append setting

--- dataset_generator.py
+++ dataset_generator.py
@@ -1,35 +1,49 @@
 from random_repo import randomRepo
 from repo_data import repoHasRelease, repoLanguage, repoReadme, repoStars
-from write_dataset import create_dataset, DATASET_NAME, DATASET_NAME_TEST
+from write_dataset import create_dataset, count_lines, DATASET_NAME, DATASET_NAME_TEST
+
+# settings
 
 TEST = True
 
 DATASET = DATASET_NAME
 ROWS = 10000
+APPEND = True
 
 DATASET_TEST = DATASET_NAME_TEST
 ROWS_TEST = 5
+APPEND_TEST = True
 
 if __name__ == "__main__":
 
     dataset = DATASET
     rows = ROWS
+    append = APPEND
     print_end = '\r' # to overwrite lines on console
     if (TEST):
         dataset = DATASET_TEST
         rows = ROWS_TEST
+        append = APPEND_TEST
         print_end = '\n'
 
-    write, close = create_dataset(dataset)
+    print("writing to " + dataset)
+    print("target csv rows: " + str(rows))
+
     i = 0
+    if (append):
+        written_rows = count_lines(dataset)
+        written_rows -= 1 # do not count header
+        i = written_rows
+        print("append mode on, " + str(written_rows) + " already in file (header excluded)")
+    else:
+        print("overwriting dataset if present")
+
+    write, close = create_dataset(dataset, append=append)
     while i < rows:
         owner, repo = randomRepo()
         language, percent = repoLanguage(owner, repo)
         stars = repoStars(owner, repo)
-        language = "a"
-        percent = 0.90
-        stars = 1000
-        if (percent > 0.85 and stars > 300):
+        if (percent > 0.80 and stars > 200):
             write(
                 owner=owner,
                 repo=repo,
@@ -39,8 +53,8 @@ if __name__ == "__main__":
                 readme=repoReadme(owner, repo)
             )
             i += 1
-            print(f"line {i} of {rows} -- written {owner}/{repo} ({stars} stars, lang {language} {percent*100}%)                                                    ", end=print_end)
+            print(f"line {i} of {rows} -- written {owner}/{repo} ({stars} stars, lang {language} {percent*100}%)                                              ", end=print_end)
         else:
-            print(f"line {i} of {rows} -- skipped {owner}/{repo} ({stars} stars, lang {language} {percent*100}%)                                                    ", end=print_end)
-    print(f"finished writing {rows} rows                                                                                                                    ")
+            print(f"line {i} of {rows} -- skipped {owner}/{repo} ({stars} stars, lang {language} {percent*100}%)                                              ", end=print_end)
+    print(f"finished writing {rows} rows                                                                                                              ")
     close()
