b3297cb96f632be74563e5efaea5c868ef634212
Edoardo Fava <f.edoardo.0@gmail.com>
2023-12-13 12:27:30 +0100

    random repo api random error handling

--- random_repo.py
+++ random_repo.py
@@ -1,10 +1,30 @@
 import requests
 import json
+import time
+
+def isRandomApiRandomError(response):
+    return (
+        response.status_code == 400 and
+        (
+            response.content.decode() == '{"error":true,"reason":"Value required for key \'items\'."}' or
+            response.content.decode() == '{"reason":"Value required for key \'items\'.","error":true}'
+        )
+    )
+
 
 def randomRepo() -> tuple[str, str]:
     response = requests.get("https://gitrandom.digitalbunker.dev/fetch?topic=Any&language=")
     if not response.ok:
-        raise Exception("GET request fail for random repo endpoint")
+        # sometimes the api randomly responds with an error, just ignore that
+        if isRandomApiRandomError(response):
+            #print("random api error, waiting 30 seconds")
+            time.sleep(30)
+            return randomRepo()
+        raise Exception(
+            "GET request fail for random repo endpoint\n" +
+            "status code: " + str(response.status_code) + "\n" +
+            "payload: " + response.content.decode()
+        )
     repoDetails = json.loads(response.content)
     return repoDetails["owner"]["login"], repoDetails["name"]
 
