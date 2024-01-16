611133a3db60ebb3fd35f40cc25be7d07cb059e4
Edoardo Fava <f.edoardo.0@gmail.com>
2024-01-16 13:16:12 +0100

    dataset_generator can choose repos source

--- dataset_generator.py
+++ dataset_generator.py
@@ -1,4 +1,5 @@
 from os.path import isfile
+from get_repo_csv import create_getRepo
 from get_repo_random import randomRepo
 from repo_data import repoHasRelease, repoLanguage, repoReadme, repoStars
 from write_dataset import create_dataset, count_lines, DATASET_NAME, DATASET_NAME_TEST
@@ -15,6 +16,10 @@ DATASET_TEST = DATASET_NAME_TEST
 ROWS_TEST = 5
 APPEND_TEST = True
 
+SOURCE_RANDOM = "random"
+SOURCE_CSV = "csv"
+SOURCE = SOURCE_CSV
+
 if __name__ == "__main__":
 
     dataset = DATASET
@@ -27,7 +32,18 @@ if __name__ == "__main__":
         append = APPEND_TEST
         print_end = '\n'
 
-    print("writing to " + dataset)
+    def getRepoInit() -> None:
+        return
+    def getRepo() -> tuple[str, str]:
+        raise Exception("not implemented for source " + SOURCE)
+    def getRepoEnd() -> None:
+        return
+    if SOURCE == SOURCE_RANDOM:
+        getRepo = randomRepo
+    elif SOURCE == SOURCE_CSV:
+        getRepo, getRepoEnd = create_getRepo()
+
+    print("writing to " + dataset + " retrieving repos with mode \"" + SOURCE + "\"")
     print("target csv rows: " + str(rows))
 
     i = 0  # counts writes to dataset
@@ -47,24 +63,30 @@ if __name__ == "__main__":
         else:
             print("creating new dataset, file: " + dataset)
 
+    getRepoInit()
     write, close = create_dataset(dataset, append=append)
     while i < rows:
-        owner, repo = randomRepo()
-        language, percent = repoLanguage(owner, repo)
-        stars = repoStars(owner, repo)
-        i0 += 1
-        if (percent > 0.80 and stars > 200):
-            write(
-                owner=owner,
-                repo=repo,
-                release=repoHasRelease(owner, repo),
-                language=language,
-                stars=stars,
-                readme=repoReadme(owner, repo)
-            )
-            i += 1
-            print(f"request {i0} - line {i} of {rows} -- written {owner}/{repo} ({stars} stars, lang {language} {percent*100}%)               ", end=print_end)
-        else:
-            print(f"request {i0} - line {i} of {rows} -- skipped {owner}/{repo} ({stars} stars, lang {language} {percent*100}%)               ", end=print_end)
-    print(f"finished writing {rows} rows                                                                               ")
+        owner, repo = getRepo()
+        try:
+            language, percent = repoLanguage(owner, repo)
+            stars = repoStars(owner, repo)
+            i0 += 1
+            if (percent > 0.80 and stars > 200):
+                write(
+                    owner=owner,
+                    repo=repo,
+                    release=repoHasRelease(owner, repo),
+                    language=language,
+                    stars=stars,
+                    readme=repoReadme(owner, repo)
+                )
+                i += 1
+                print(f"request {i0} - line {i}/{rows} -- written {owner}/{repo} ({stars} stars, lang {language} {percent*100}%)               ", end=print_end)
+            else:
+                print(f"request {i0} - line {i}/{rows} -- skipped {owner}/{repo} ({stars} stars, lang {language} {percent*100}%)               ", end=print_end)
+        except LookupError as e:
+            # repository data not found, may be deleted
+            print(f"request {i0} - line {i}/{rows} -- skipped {owner}/{repo} (not found)                                                   ", end=print_end)
+    getRepoEnd()
+    print(f"finished writing {rows} rows                                                                            ")
     close()
